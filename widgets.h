4ecc81bbcbda5e7f1c75ef8535a96f4bff135ebe
mike7d7 <mike7d7@proton.me>
2025-06-08 09:52:50 -0600

    widgets.h: remove unused array

--- widgets.h
+++ widgets.h
@@ -211,7 +211,6 @@ void HeaderBar() {
   };
 }
 
-int test_array[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 void ImageGrid(SDL_Texture **img) {
   CLAY({.id = CLAY_ID("image_grid"),
         .clip = {.vertical = true, .childOffset = Clay_GetScrollOffset()},
